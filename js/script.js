946ad6c6a325fd16f67320d173ee8f2b3dd3b910
Jake Zeledon <jackiezeledon@gmail.com>
2024-05-06 23:20:23 -0600

    Add dark mode

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@
 //         window.removeEventListener("mousemove", updateMousePosition);
 //     };
 // });
+
+const sunButton = document.getElementById('sun-button');
+const sunIcon = document.getElementById('sun-icon');
+const body = document.getElementById('body');
+
+sunButton.addEventListener('click', function() {
+    if (sunIcon.src.includes('img/theme-light.svg')) {
+        sunIcon.src = 'img/theme-dark.svg';
+        body.classList.add('dark-mode');
+    } else {
+        sunIcon.src = 'img/theme-light.svg'; 
+        body.classList.remove('dark-mode');
+    }
+});
