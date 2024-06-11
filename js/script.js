1116018f5f7f2504c37fe0ba26e3ccd5a4036796
Jake Zeledon <jackiezeledon@gmail.com>
2024-06-11 12:52:32 -0600

    Add navbar functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-CXDB014HWL');
+
+const sunButton = document.getElementById('sun-button');
+const sunIcon = document.getElementById('sun-icon');
+const body = document.getElementById('body');
+
+
 window.addEventListener("load", function() {
     const hero = document.getElementById("hero");
 
@@ -14,16 +25,6 @@ window.addEventListener("load", function() {
     };
 });
 
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-CXDB014HWL');
-
-const sunButton = document.getElementById('sun-button');
-const sunIcon = document.getElementById('sun-icon');
-const body = document.getElementById('body');
-
 sunButton.addEventListener('click', function() {
     if (sunIcon.src.includes('img/theme-light.svg')) {
         sunIcon.src = 'img/theme-dark.svg';
@@ -32,4 +33,36 @@ sunButton.addEventListener('click', function() {
         sunIcon.src = 'img/theme-light.svg'; 
         body.classList.remove('dark-mode');
     }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.main-link');
+    const sections = document.querySelectorAll('section');
+
+    // Function to highlight the active link based on the section in view
+    function highlightActiveLink() {
+        let currentSection = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                currentSection = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href').substring(1) === currentSection) {
+                link.classList.add('active');
+            }
+        });
+    }
+
+    // Initial call to highlight the active link
+    highlightActiveLink();
+
+    // Event listener for scroll to update the active link
+    document.addEventListener('scroll', highlightActiveLink);
 });
