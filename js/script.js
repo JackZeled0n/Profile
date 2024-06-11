6384f3477cad6f4962f86139f78c9fb05e6ebe7e
Jake Zeledon <jackiezeledon@gmail.com>
2024-06-11 16:13:53 -0600

    Add back to top functionality

--- js/script.js
+++ js/script.js
@@ -66,3 +66,27 @@ document.addEventListener('DOMContentLoaded', function() {
     // Event listener for scroll to update the active link
     document.addEventListener('scroll', highlightActiveLink);
 });
+
+
+let mybutton = document.getElementById("btn_back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
